e82aef644d7d94aeeeba3a9df2036459cd5a1b38
Thiago Andrade <thiagogpa@gmail.com>
2020-06-13 19:57:22 -0400

    structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Toast from "react-bootstrap/Toast";
@@ -6,35 +6,41 @@ import Container from "react-bootstrap/Container";
 
 import "./App.css";
 
-function App() {
-  const ExampleToast = ({ children }) => {
-    const [show, toggleShow] = useState(true);
+export class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const ExampleToast = ({ children }) => {
+      const [show, toggleShow] = useState(true);
+
+      return (
+        <Toast show={show} onClose={() => toggleShow(!show)}>
+          <Toast.Header>
+            <strong className="mr-auto">React-Bootstrap</strong>
+          </Toast.Header>
+          <Toast.Body>{children}</Toast.Body>
+        </Toast>
+      );
+    };
 
     return (
-      <Toast show={show} onClose={() => toggleShow(!show)}>
-        <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
+      <div className="App">
+        <Container className="p-3">
+          <Jumbotron>
+            <h1 className="header">Welcome To React-Bootstrap</h1>
+            <ExampleToast>
+              We now have Toasts
+              <span role="img" aria-label="tada">
+                🎉
+              </span>
+            </ExampleToast>
+          </Jumbotron>
+        </Container>
+      </div>
     );
-  };
-
-  return (
-    <div className="App">
-      <Container className="p-3">
-        <Jumbotron>
-          <h1 className="header">Welcome To React-Bootstrap</h1>
-          <ExampleToast>
-            We now have Toasts
-            <span role="img" aria-label="tada">
-              🎉
-            </span>
-          </ExampleToast>
-        </Jumbotron>
-      </Container>
-    </div>
-  );
+  }
 }
 
 export default App;
