17ec0636fc4891f583274318eb8cf5242c175611
Thiago Andrade <thiagogpa@gmail.com>
2020-06-13 21:59:41 -0400

    Initial menus

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
-import React, { Component, useState } from "react";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navigation from "./pages/Navigation.js";
+import PageNotFound from "./pages/PageNotFound.js";
+import Home from "./pages/Home.js";
+import Example from "./pages/ExampleBootstrap.js";
+import FAQ from "./pages/FAQ.js";
 
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Toast from "react-bootstrap/Toast";
-import Container from "react-bootstrap/Container";
-
-import "./App.css";
-
-export class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    const ExampleToast = ({ children }) => {
-      const [show, toggleShow] = useState(true);
-
-      return (
-        <Toast show={show} onClose={() => toggleShow(!show)}>
-          <Toast.Header>
-            <strong className="mr-auto">React-Bootstrap</strong>
-          </Toast.Header>
-          <Toast.Body>{children}</Toast.Body>
-        </Toast>
-      );
-    };
-
-    return (
-      <div className="App">
-        <Container className="p-3">
-          <Jumbotron>
-            <h1 className="header">Welcome To React-Bootstrap</h1>
-            <ExampleToast>
-              We now have Toasts
-              <span role="img" aria-label="tada">
-                🎉
-              </span>
-            </ExampleToast>
-          </Jumbotron>
-        </Container>
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="innerContent">
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/example" component={Example} />
+          <Route path="/faq" component={FAQ} />
+          <Route component={PageNotFound} />
+        </Switch>
       </div>
-    );
-  }
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/ExampleBootstrap.js
@@ -0,0 +1,44 @@
+import React, { Component, useState } from "react";
+
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Toast from "react-bootstrap/Toast";
+import Container from "react-bootstrap/Container";
+
+export class Example extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const ExampleToast = ({ children }) => {
+      const [show, toggleShow] = useState(true);
+
+      return (
+        <Toast show={show} onClose={() => toggleShow(!show)}>
+          <Toast.Header>
+            <strong className="mr-auto">React-Bootstrap</strong>
+          </Toast.Header>
+          <Toast.Body>{children}</Toast.Body>
+        </Toast>
+      );
+    };
+
+    return (
+      <div className="App">
+        <Container className="p-3">
+          <Jumbotron>
+            <h1 className="header">Welcome To React-Bootstrap</h1>
+            <ExampleToast>
+              We now have Toasts
+              <span role="img" aria-label="tada">
+                🎉
+              </span>
+            </ExampleToast>
+          </Jumbotron>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Example;
