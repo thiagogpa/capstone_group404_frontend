bf777863d644392ce8585adb443fd5d6aded2929
Thiago Andrade <thiagogpa@gmail.com>
2020-06-13 23:50:29 -0400

    Faq page
    Faq page with fixed content

--- src/components/FAQ/faq-component.jsx
+++ src/components/FAQ/faq-component.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Accordion, Card } from "react-bootstrap";
+import "./faq-component-style.css";
 
-function FAQ() {
-    return (
-        <div>
-            
-        </div>
-    )
+export function FaqCard(props) {
+  return (
+    <Card>
+      <Accordion.Toggle as={Card.Header} eventKey={props.question.id}>
+        {props.question.name}
+      </Accordion.Toggle>
+      <Accordion.Collapse eventKey={props.question.id}>
+        <Card.Body>{props.question.answer}</Card.Body>
+      </Accordion.Collapse>
+    </Card>
+  );
 }
 
-export default faq-component
+function FaqCardList(props) {
+  return (
+    <div className="faq-list">
+      <Accordion defaultActiveKey="0">
+        {props.questions.map((question) => (
+          <FaqCard key={question.id} question={question} />
+        ))}
+      </Accordion>
+    </div>
+  );
+}
+
+export default FaqCardList;
