e052a6ee4ae3cc2b1b236a4b7d28811bb315689d
Marysha <n01355798@humbermail.ca>
2020-06-15 22:43:02 -0400

    changed to inject Nvigation coponents

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,34 +1,13 @@
 import React from "react";
-import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+import HeaderNav from '../components/Navigation/HeaderNav.js'
+import FooterNav from '../components/Navigation/FooterNav.js'
 
 function Navigation() {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/home">Wall-Up Bin Rental</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/example">Bootstrap Example</Nav.Link>
-          <Nav.Link href="/faq">FAQ</Nav.Link>
-          <Nav.Link href="/contact">Contact us</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+    <HeaderNav />
+    <FooterNav />
+    </>   
   );
 }
 
