9b8e61947f535e42e81317e600f8e476e387fc7e
Thiago Andrade <thiagogpa@gmail.com>
2020-06-22 21:44:07 -0400

    test

--- src/views/pages/login/Login.jsx
+++ src/views/pages/login/Login.jsx
@@ -1,50 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Login extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      username : '',
-      password: ''
+      username: "",
+      password: "",
     };
   }
 
   handleInputChange = (event) => {
     const { value, name } = event.target;
     this.setState({
-      [name]: value
+      [name]: value,
     });
-  }
+  };
 
   onSubmit = (event) => {
     event.preventDefault();
-    fetch('http://localhost:8080/api/authenticate', {
-      method: 'POST',
+    fetch("http://localhost:8080/api/authenticate", {
+      method: "POST",
       body: JSON.stringify(this.state),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     })
-    .then(res => {
-      if (res.status === 200) {
-        this.props.history.push('/');
-      } else {
-        const error = new Error(res.error);
-        throw error;
-      }
-    })
-    .catch(err => {
-      console.error(err);
-      alert('Error logging in please try again');
-    });
-  }
+      .then((res) => {
+        if (res.status === 200) {
+          console.log("200");
+          console.log(res);
+          this.props.history.push("/");
+        } else {
+          console.log(res);
+          console.log("other");
+          const error = new Error(res.error);
+          throw error;
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Error logging in please try again");
+      });
+  };
 
   render() {
     return (
       <form onSubmit={this.onSubmit}>
         <h1>Login Below!</h1>
         <input
-          name="email"
+          type="username"
+          name="username"
           placeholder="Enter email"
           value={this.state.username}
           onChange={this.handleInputChange}
@@ -52,12 +57,13 @@ export default class Login extends Component {
         />
         <input
           type="password"
+          name="password"
           placeholder="Enter password"
           value={this.state.password}
           onChange={this.handleInputChange}
           required
         />
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Submit" />
       </form>
     );
   }
