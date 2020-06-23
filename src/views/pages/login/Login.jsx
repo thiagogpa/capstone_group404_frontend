599b7df6dde6d49c15cf2c67a818df5c0f379bf2
Thiago Andrade <thiagogpa@gmail.com>
2020-06-22 23:53:25 -0400

    Login update
    adds login, but still not working with cookies properly

---
+++ src/views/pages/login/Login.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:8080/api/authenticate", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          console.log("200");
+          console.log(res);
+          this.props.history.push("/");
+        } else {
+          console.log(res);
+          console.log("other");
+          const error = new Error(res.error);
+          throw error;
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Error logging in please try again");
+      });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Login Below!</h1>
+        <input
+          type="username"
+          name="username"
+          placeholder="Enter email"
+          value={this.state.username}
+          onChange={this.handleInputChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          value={this.state.password}
+          onChange={this.handleInputChange}
+          required
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
